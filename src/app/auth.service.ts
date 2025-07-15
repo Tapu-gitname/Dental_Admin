c73a28f93cfaee704dc055eb6048402f40d5feb8
Tapu-gitname <sahusoumyaranjan2@gmail.com>
2025-07-15 09:14:41 +0530

    added buildspec.yml file

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment.staging';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
